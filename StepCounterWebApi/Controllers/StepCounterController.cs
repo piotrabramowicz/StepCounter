bec710ac508b4d1b300ed4225d6764355260ccd8
piotrabramowicz <61869081+piotrabramowicz@users.noreply.github.com>
2024-11-29 16:27:48 +0100

    Add Swagger specificaton

--- StepCounterWebApi/Controllers/StepCounterController.cs
+++ StepCounterWebApi/Controllers/StepCounterController.cs
@@ -11,79 +11,144 @@ namespace StepCounterWebApi.Controllers
     {
         private readonly IStepCounterRepository stepCounterRepository = new StepCounterRepository(FakeDataContext.GetInstance());
 
-        //1. As a User
-        //I want to be able to create a new counter
-        //So that steps can be accumulated for a team of one or multiple employees
+        /// <summary>
+        ///     Creates new team with members
+        /// </summary>
+        /// <param name="teamName">team name</param>
+        /// <param name="memberNames"> names of members</param>
+        /// <remarks>
+        ///     Sample request:
+        ///
+        ///        POST /StepCounter/CreateNewTeamWithMembers
+        ///         {
+        ///            "teamName": "Best Team",
+        ///            "memberNames": { "member 1", "member 2", "member 3",}
+        ///         }
+        ///
+        /// </remarks>
+        /// <response code="200">Counter created</response>
+        /// <response code="500">Oops! Not today.</response>
         [HttpPost()]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(500)]
         public void CreateNewTeamWithMembers(string teamName, string[] memberNames)
         {
             stepCounterRepository.CreateTeamWithMembers(teamName, memberNames);
         }
 
-        //2. As a User
-        //I want to be able to increment the value of a stored counter
-        //So that I can get steps counted towards my team's score
+        /// <summary>
+        /// Report new step of employee
+        /// </summary>
+        /// <param name="memberId">Member ID</param>
+        /// <response code="200">Counter inreased</response>
+        /// <response code="500">Oops! Not today.</response>
         [HttpPut()]
-        public void IncrementCounter(string counterId)
+        [ProducesResponseType(200)]
+        [ProducesResponseType(500)]
+        public void IncrementCounter(string memberId)
         {
-            stepCounterRepository.IncreaseCounter(ParseGuid(counterId));
+            stepCounterRepository.IncreaseCounter(ParseGuid(memberId));
         }
 
-        //3. As a User
-        //I want to get the current total steps taken by a team
-        //So that I can see how much that team have walked in total
+        /// <summary>
+        /// Get list of steps of all members for a team
+        /// </summary>
+        /// <param name="teamId">Team ID</param>
+        /// <returns></returns>
+        /// <response code="200">ok</response>
+        /// <response code="500">Oops! Not today.</response>
         [HttpGet()]
+        [ProducesResponseType(typeof(Team), 200)]
+        [ProducesResponseType(500)]
         public Team GetTotalStepsForTeam(string teamId)
         {
             return stepCounterRepository.GetTotalStepsForTeam(ParseGuid(teamId));
         }
 
-        //4. As a User
-        //I want to list all teams and see their step counts
-        //So that I can compare my team with the others
+        /// <summary>
+        /// Get list of steps of all members of all teams
+        /// </summary>
+        /// <returns></returns>
+        /// <response code="200">ok</response>
+        /// <response code="500">Oops! Not today.</response>
         [HttpGet()]
+        [ProducesResponseType(typeof(IEnumerable<Team>), 200)]
+        [ProducesResponseType(500)]
         public IEnumerable<Team> GetTotalStepsByAllTeams()
         {
             return stepCounterRepository.GetTotalStepsForAllTeams();
         }
 
-        //5. As a User
-        //I want to list all counters in a team
-        //So that I can see how much each team member have walked
+        /// <summary>
+        /// Get list of members for a team
+        /// </summary>
+        /// <param name="teamId"></param>
+        /// <returns></returns>
+        /// <response code="200">ok</response>
+        /// <response code="500">Oops! Not today.</response>
         [HttpGet()]
+        [ProducesResponseType(typeof(IEnumerable<TeamMember>), 200)]
+        [ProducesResponseType(500)]
         public IEnumerable<TeamMember> GetMembersForTeam(string teamId)
         {
             return stepCounterRepository.GetMembersForTeam(ParseGuid(teamId));
         }
 
-        //6. As a User
-        //I want to be able to add/delete teams
-        //So that I can manage teams
+        /// <summary>
+        /// Add team
+        /// </summary>
+        /// <param name="teamId">team ID</param>
+        /// <response code="200">ok</response>
+        /// <response code="500">Oops! Not today.</response>
         [HttpPost()]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(500)]
         public void AddTeam(string teamId)
         {
             stepCounterRepository.DeleteTeam(ParseGuid(teamId));
         }
 
+        /// <summary>
+        /// Delete team 
+        /// </summary>
+        /// <param name="teamId">Team ID</param>
+        /// <response code="200">ok</response>
+        /// <response code="500">Oops! Not today.</response>
         [HttpDelete()]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(500)]
         public void DeleteTeam(string teamId)
         {
             stepCounterRepository.DeleteTeam(ParseGuid(teamId));
         }
 
-        //7. As a User
-        //I want to be able to add/delete counters
-        //So that I can manage team member's counters
+        /// <summary>
+        /// Add new member
+        /// </summary>
+        /// <param name="teamId">Team ID</param>
+        /// <param name="memberName">Name of member</param>
+        /// <response code="200">ok</response>
+        /// <response code="500">Oops! Not today.</response>
         [HttpPost()]
-        public void AddNewMemberToTeam(string teamId, string memberName)
+        [ProducesResponseType(200)]
+        [ProducesResponseType(500)]
+        public void AddNewMember(string teamId, string memberName)
         {
             stepCounterRepository.AddNewMember(ParseGuid(teamId), memberName);
         }
 
+        /// <summary>
+        /// Delete team member
+        /// </summary>
+        /// <param name="memberId">Member ID</param>
+        /// <response code="200">ok</response>
+        /// <response code="500">Oops! Not today.</response>
         [HttpDelete()]
-        public void DeleteTeamMember(string counterId)
+        [ProducesResponseType(200)]
+        [ProducesResponseType(500)]
+        public void DeleteTeamMember(string memberId)
         {
-            stepCounterRepository.DeleteMember(ParseGuid(counterId));
+            stepCounterRepository.DeleteMember(ParseGuid(memberId));
         }
 
         private static Guid ParseGuid(string id) => Guid.Parse(id);
--- StepCounterWebApi/Program.cs
+++ StepCounterWebApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,11 +8,36 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+                                {
+                                    options.SwaggerDoc("v1", new OpenApiInfo
+                                    {
+                                        Version = "v1",
+                                        Title = "Step Counter API",
+                                        Description = "count steps of your team mates ;)",
+                                        TermsOfService = new Uri("https://example.com/terms"),
+                                        Contact = new OpenApiContact
+                                        {
+                                            Name = "Example Contact",
+                                            Url = new Uri("https://example.com/contact")
+                                        },
+                                        License = new OpenApiLicense
+                                        {
+                                            Name = "Example License",
+                                            Url = new Uri("https://example.com/license")
+                                        }
+                                    });
+
+                                    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                                    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+                                });
 
 var app = builder.Build();
 
-app.UseSwagger();
+app.UseSwagger(options =>
+                {
+                    options.SerializeAsV2 = true;
+                });
 app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
